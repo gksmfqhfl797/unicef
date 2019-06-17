2817616f8599cf7a42d3f9845fa48bb077325fff
Wise Kim <wisekim@wisekimui-MacBook-Pro.local>
2019-06-17 23:11:57 +0900

    Updates

---
+++ precache-manifest.66cbb3af09b5ff5d62f0b98ae8d4feeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251bcc9f910b1b5d1d51d287bc5c7a95",
+    "url": "/unicef/index.html"
+  },
+  {
+    "revision": "f9e418b8989c445903c5",
+    "url": "/unicef/static/css/main.98167453.chunk.css"
+  },
+  {
+    "revision": "f98c90c4dc4d7aaa0a91",
+    "url": "/unicef/static/js/2.714a5e69.chunk.js"
+  },
+  {
+    "revision": "f9e418b8989c445903c5",
+    "url": "/unicef/static/js/main.6dee793e.chunk.js"
+  },
+  {
+    "revision": "b1b4edea72eb5b104e0f",
+    "url": "/unicef/static/js/runtime~main.a51e5f95.js"
+  },
+  {
+    "revision": "d5fc7125b6103f84c9380ed4de7f068f",
+    "url": "/unicef/static/media/unicef.d5fc7125.jpg"
+  }
+]);
